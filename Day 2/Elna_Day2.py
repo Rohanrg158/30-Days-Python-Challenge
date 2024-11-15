541ca237b448e0a2c7c8e50483d3a2c0da29ef52
elnabittaj <elnabittaj@gmail.com>
2024-11-15 08:27:29 +0530

    Add files via upload

---
+++ Day 2/Elna_Day2.py
@@ -0,0 +1,20 @@
+num1=int(input("Enter a number"))
+num2=int(input("Enter another number"))
+
+sum=num1+num2
+print(sum)
+
+difference=num1-num2
+print(difference)
+
+product=num1*num2
+print(product)
+
+quotient=num1/num2
+print(quotient)
+
+floor_div=num1//num2
+print(floor_div)
+
+remainder=num1%num2
+print(remainder)
