dc475b486d23195d4cf344c807497ca00f203426
nayanavnair01 <nayananair2006@gmail.com>
2024-11-16 18:36:12 +0530

    Add files via upload

---
+++ Day 2/nayana_day2.py
@@ -0,0 +1,18 @@
+num1=int(input("Enter a number"))
+num2=int(input("Enter another number"))
+print("sum =",num1+num2)
+
+difference = num1-num2
+print("difference=",difference)
+
+product = num1*num2
+print("product=",product)
+
+quotient= num1/num2
+print("quotient=",quotient)
+
+floor_division= num1//num2
+print("floor div=",floor_division)
+
+modulo= num1%num2
+print("modulo=",modulo)
