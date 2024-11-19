6ca10ce5b33b4e7135cec3170956e6b017ddc2ea
RRG <rohan.renji158@gmail.com>
2024-11-19 22:44:56 +0530

    Add files via upload

---
+++ Day 6/Rohan_Day6.py
@@ -0,0 +1,5 @@
+for i in range(1, 21):  # Loop through numbers from 1 to 20
+    if i == 12:
+        continue  # Skip the number 12
+    if i % 2 == 0:  # Check if the number is even
+        print(i)  # Print the even number
