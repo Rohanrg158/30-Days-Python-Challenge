ed7f7733d8e2173c6d4fa191d93f9a942c76bce8
elnabittaj <elnabittaj@gmail.com>
2024-11-30 07:53:48 +0530

    Add files via upload

---
+++ Day 7/Elna_day7.py
@@ -0,0 +1,6 @@
+def find_even_numbers(input_list):
+    even_numbers=[num for num in input_list if num % 2==0]
+    return even_numbers
+input_list=[1,2,3,4,5,6,7,8]
+result=find_even_numbers(input_list)
+print(result)
